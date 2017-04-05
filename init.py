e30a4623464da9f738a6ad865b54a74abdb5f8fe
Alexey Makurin <rufige@yandex.ru>
2017-04-05 19:01:07 +0300

    maxpooling remove

--- init.py
+++ init.py
@@ -84,7 +84,7 @@ def create_model(input_shape= (160,320,3)):
 	
 	model.add(Conv2D(24,5, strides=(2, 2)))
 	model.add(Activation('relu'))
-	model.add(MaxPooling2D())
+	#model.add(MaxPooling2D())
 	model.add(Dropout(0.5))
 	
 	model.add(Conv2D(36,5, strides=(2, 2)))
