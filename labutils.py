11e9b29dd3ec7961ce87e7724f3172dd4a7ea0a6
Alexey Makurin <rufige@yandex.ru>
2017-04-05 20:44:05 +0300

    rstructuring

--- labutils.py
+++ labutils.py
@@ -5,7 +5,13 @@ import math
 def tocv(npshape):
 	height, width = npshape
 	return (width, height)
-	
+
+def random_flip(img, angle):
+	if np.random.randint(0,2)==0:
+		img = cv2.flip(img, 1)
+		angle = -1.0 * angle
+	return img, angle
+
 def crop(img, topbottom_leftright):
 	top = topbottom_leftright[0][0]
 	bottom = img.shape[0]- topbottom_leftright[0][1]
@@ -47,3 +53,61 @@ def randomize_light(img, const_factor = 0.5):
 	L[L>255] = 255
 	lab = cv2.merge([np.array(L, dtype = np.uint8),a,b])
 	return cv2.cvtColor(lab, cv2.COLOR_Lab2RGB)
+
+def readlog(log_path = './data/driving_log.csv', img_path = './data/IMG/'):
+	lines = []
+	with open(log_path) as csvlog:
+		reader = csv.reader(csvlog)
+		for line in reader:
+			for i in range(0,3):
+				line[i] = img_path + line[i].split('\\')[-1]
+			for i in range(3,7):
+				line[i] = float(line[i])
+			lines.append(line)
+	return lines;
+
+def get_sample(log_line, keep_direct_threshold = 0.1, 
+			direct_threshold = 0.1, side_angle = 0.2):
+	index = 0
+	add = 0
+	angle = log_line[3] 
+	if angle < direct_threshold:
+		if np.random.uniform() > keep_direct_threshold:
+			# steering additions center, left, right
+			diffs = [0, side_angle, -1.0 *side_angle]
+			index = np.random.randint(1,3)
+			add = diffs[index] 
+	img_path = log_line[index]
+	img = cv2.imread(img_path)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	angle = angle + add 
+	return (img, angle)
+
+#log = readlog()	
+#img, angle = get_sample(log[0])
+#img, angle = random_flip(img, angle)
+#
+#cv2.imshow('angle {}'.format(angle),img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#
+#img = distort(img)
+#cv2.imshow('distort {}'.format(angle),img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#
+#img = randomize_light(img)
+#cv2.imshow('light {}'.format(angle),img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#
+#img = crop(img, ((70, 25), (0, 0)))
+#cv2.imshow('crop {}'.format(angle),img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#
+#img = resize(img, (32,128))
+#cv2.imshow('resize {}'.format(angle),img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#print (img.shape)
---
+++ model.py
@@ -0,0 +1,114 @@
+import csv
+import cv2
+import random
+import numpy as np
+import sklearn
+import labutils as lu
+from sklearn.model_selection import train_test_split
+
+def train_valid_split(samples):
+	return train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=128, 
+			keep_direct_threshold = 1., direct_threshold = 0.1, side_angle = 0.2, 
+			flip_random = False, 
+			add_distortion = False,
+			randomize_light = False,
+			resize_param = None, crop_param = None):
+	num_samples = len(samples)
+	while 1:
+		random.shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
+			images = []
+			angles = []
+			for batch_sample in batch_samples:
+				img, angle = lu.get_sample(batch_sample, 
+										keep_direct_threshold=keep_direct_threshold,
+										direct_threshold=direct_threshold,
+										side_angle=side_angle)
+				if flip_random:
+					img, angle = lu.random_flip(img, angle)
+				if crop_param is not None:
+					img = lu.crop(img, crop_param)	
+				if add_distortion:
+					img = lu.distort(img)
+				if randomize_light:
+					img = lu.randomize_light(img)
+				if resize_param is not None:
+					img = lu.resize(img, resize_param)
+				images.append(img)
+				angles.append(angle)
+			X_train = np.array(images)
+			y_train = np.array(angles)
+			yield sklearn.utils.shuffle(X_train, y_train)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, Activation
+
+def create_model(input_shape= (160,320,3)):
+	model = Sequential()
+	model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=input_shape))
+	model.add(Conv2D(3,1))
+	
+	model.add(Conv2D(24,5, strides=(2, 2)))
+	model.add(Activation('relu'))
+	#model.add(MaxPooling2D())
+	model.add(Dropout(0.5))
+	
+	model.add(Conv2D(36,5, strides=(2, 2)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D())
+	model.add(Dropout(0.5))
+	
+	model.add(Conv2D(48,3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D())
+	model.add(Dropout(0.5))
+	
+	model.add(Flatten())
+	model.add(Dense(512))
+	model.add(Dropout(0.5))
+	model.add(Activation('relu'))
+	model.add(Dense(128))
+	model.add(Dropout(0.5))
+	model.add(Activation('relu'))
+	model.add(Dense(16))
+	model.add(Activation('relu'))
+	model.add(Dense(1))
+	
+	return model
+
+def compile_model(model):
+	model.compile(loss='mse', optimizer='adam')
+	return model
+
+def train_model(model_file_name='model.h5',
+				log_path = './data/driving_log.csv', 
+				img_path = './data/IMG/'):
+	log = lu.readlog(log_path=log_path, img_path=img_path)
+	train_log, valid_log = train_valid_split(log)
+
+	new_shape = (65,320,3)#(32,128,3)
+	crop_param = ((70, 25), (0, 0))
+	resize_param = new_shape[:2]
+	train_generator = generator(train_log, 
+								keep_direct_threshold = 0.5, 
+								flip_random = True,
+								#resize_param=resize_param, 
+								crop_param=crop_param,
+								add_distortion=True,
+								randomize_light=True)
+	valid_generator = generator(valid_log, 
+								#resize_param=resize_param, 
+								crop_param=crop_param)
+
+	model = create_model(input_shape= new_shape)
+	model = compile_model(model)
+	model.fit_generator(train_generator, 
+						steps_per_epoch = 500, 
+						validation_data=valid_generator, 
+						validation_steps=20, epochs=5)
+
+	model.save(model_file_name)
+
