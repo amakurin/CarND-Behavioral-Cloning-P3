dae0becbd1521bcdf23193f2f5092b7665b933b6
Alexey Makurin <rufige@yandex.ru>
2017-04-05 15:37:42 +0300

    dim fix

--- init.py
+++ init.py
@@ -18,12 +18,6 @@ def readlog(log_path = './data/driving_log.csv', img_path = './data/IMG/'):
 			lines.append(line)
 	return lines;
 
-def random_flip(img, angle):
-	if np.random.randint(0,2)==0:
-		img = cv2.flip(img, 1)
-		angle = -1.0 * angle
-	return img, angle
-	
 def get_sample(log_line, use_side_angle = 0.2):
 	index = 0
 	add = 0
@@ -36,9 +30,16 @@ def get_sample(log_line, use_side_angle = 0.2):
 	img = cv2.imread(img_path)
 	angle = log_line[3] + add 
 	return (img, angle)
-	
+
+def random_flip(img, angle):
+	if np.random.randint(0,2)==0:
+		img = cv2.flip(img, 1)
+		angle = -1.0 * angle
+	return img, angle
+
 def generator(samples, batch_size=128, 
-			use_side_angle = None, flip_random = False,
+			use_side_angle = None, flip_random = False, 
+			add_distortion = False,
 			resize_param = None, crop_param = None):
 	num_samples = len(samples)
 	while 1:
@@ -53,6 +54,8 @@ def generator(samples, batch_size=128,
 					img, angle = random_flip(img, angle)
 				if crop_param is not None:
 					img = lu.crop(img, crop_param)	
+				if add_distortion:
+					img = lu.distort(img)
 				if resize_param is not None:
 					img = lu.resize(img, resize_param)
 				images.append(img)
@@ -92,14 +95,18 @@ log = readlog()
 from sklearn.model_selection import train_test_split
 train_log, valid_log = train_test_split(log, test_size=0.2)
 
-new_shape = (128,32,3)
+new_shape = (32,128,3)
 crop_param = ((70, 25), (0, 0))
-resize_param = (new_shape[1], new_shape[0])
+resize_param = new_shape[:2]
 train_generator = generator(train_log, 
 							use_side_angle = 0.2, flip_random = True,
 							resize_param=resize_param, 
-							crop_param=crop_param)
-valid_generator = generator(valid_log, resize_param=resize_param, crop_param=crop_param)
+							crop_param=crop_param
+							)
+valid_generator = generator(valid_log, 
+							resize_param=resize_param, 
+							crop_param=crop_param
+							)
 
 model = create_model(input_shape= new_shape)
 model.fit_generator(train_generator, 
@@ -116,12 +123,19 @@ model.save('model.h5')
 #cv2.imshow('angle {}'.format(angle),img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-#img = crop(img, ((70, 25), (0, 0)))
-#cv2.imshow('angle {}'.format(angle),img)
+#
+#img = lu.distort(img)
+#cv2.imshow('distort {}'.format(angle),img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 #
-#img = resize(img, (32,32))
-#cv2.imshow('angle {}'.format(angle),img)
+#img = lu.crop(img, ((70, 25), (0, 0)))
+#cv2.imshow('crop {}'.format(angle),img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
+#
+#img = lu.resize(img, (32,128))
+#cv2.imshow('resize {}'.format(angle),img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#print (img.shape)
--- labutils.py
+++ labutils.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
+import math
 
+def tocv(npshape):
+	height, width = npshape
+	return (width, height)
+	
 def crop(img, topbottom_leftright):
 	top = topbottom_leftright[0][0]
 	bottom = img.shape[0]- topbottom_leftright[0][1]
@@ -9,4 +14,25 @@ def crop(img, topbottom_leftright):
 	return img[top:bottom, left:right]
 
 def resize(img, new_shape):
-	return cv2.resize(img, new_shape, interpolation=cv2.INTER_AREA)
+	cvshape = tocv(new_shape[:2])
+	return cv2.resize(img, cvshape, interpolation=cv2.INTER_AREA)
+	
+def distort(img, maxAngle = np.pi/18, maxCenterDistance = 6, maxScale = 1.2):
+	'''
+	Adds distortion to input img
+	img - numpy array (height, weight, chans)
+	maxAngle - maximum absolute angle, will be used to select random from [-maxAngle,maxAngle] uniformely
+	maxCenterDistance - maximum distance to move center of rotation
+	maxScale - maximum scale
+	'''
+	img_shape = img.shape[:2]
+	cvshape = tocv(img_shape)
+	dangle = np.random.uniform(-maxAngle, maxAngle, 1)[0]
+	distance = math.sqrt(maxCenterDistance)
+	dx = math.floor(np.random.uniform(-distance, distance, 1)[0])
+	dy = math.floor(np.random.uniform(-distance, distance, 1)[0])
+	dscale = np.random.uniform(2-maxScale, maxScale, 1)[0]
+	dcenter = np.array(cvshape)/2 +[dx,dy]
+	rot_mat = cv2.getRotationMatrix2D(tuple(dcenter), dangle, dscale)
+	result = cv2.warpAffine(img, rot_mat, cvshape, flags=cv2.INTER_LINEAR)
+	return result	
